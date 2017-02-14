6b166d3a28d3ad56be09716c57a1df25b76a42ba
TimingJL <eefozeo@yahoo.com.tw>
2017-02-14 12:32:47 +0800

    item image url can be empty

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
 	belongs_to :restaurant
-	validates_presence_of :name, :imageURL, :price # Can't empty.
+	validates_presence_of :name, :price # Can't empty. , :imageURL
 end
